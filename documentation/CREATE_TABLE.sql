170ad7f4236e0e930b9c6d596662823db99ad5ea
Niko <netkaartinen@gmail.com>
2017-02-16 16:51:49 +0200

    Basic form and printing functionalities added.

---
+++ documentation/CREATE_TABLE.sql
@@ -0,0 +1,28 @@
+CREATE TABLE user (
+id INT NOT NULL AUTO_INCREMENT,
+username VARCHAR(255),
+password VARCHAR(255),
+PRIMARY KEY (id)
+)Engine=InnoDB;
+
+CREATE TABLE vehicle (
+id INT NOT NULL AUTO_INCREMENT,
+userid INT NOT NULL,
+type INT NOT NULL,
+displayname VARCHAR(255),
+totalkilometersdriven INT,
+latestinspection DATE,
+lastinspection DATE,
+latestoilchange DATE,
+PRIMARY KEY (id),
+FOREIGN KEY (userid) REFERENCES user (id)
+)Engine=InnoDB;
+
+CREATE TABLE servicelog (
+id INT NOT NULL AUTO_INCREMENT,
+vehicleid INT NOT NULL,
+entry TEXT,
+date TEXT,
+PRIMARY KEY (id),
+FOREIGN KEY (vehicleid) REFERENCES vehicle (id)
+)Engine=InnoDB;
--- src/main/java/fi/kaamos/controller/VehicleController.java
+++ src/main/java/fi/kaamos/controller/VehicleController.java
@@ -2,22 +2,27 @@ package fi.kaamos.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import fi.kaamos.bean.Vehicle;
+
+
 
 @Controller
 public class VehicleController {
 	
-	@RequestMapping("/")
-	public String vehicle(Model model) {
-		Vehicle v = new Vehicle();
-		v.setId(1);
-		v.setUserId(1);
-		v.setType(1);
-		v.setDisplayName("Toyota Corolla '94");
-		v.setTotalKilometersDriven(12);
-		model.addAttribute("vehicle", v);
+	@RequestMapping(value="/", method=RequestMethod.GET)
+	public String vehicleForm(Model model) {
+		model.addAttribute("vehicle", new Vehicle());
 		return "index";
+	}
+	
+	@RequestMapping(value="/", method=RequestMethod.POST)
+	public String vehicleSubmit(@ModelAttribute Vehicle vehicle, Model model) {
+		System.out.println(vehicle.getDisplayName());
+		model.addAttribute("vehicle", vehicle);
+		return "result";
 	}
 }
